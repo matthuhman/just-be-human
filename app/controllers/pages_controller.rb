d88348da5a90dbf80d17e5c819e078ace5cde2b5
matthuhman <huhmanbeing@gmail.com>
2019-08-04 21:44:25 -0600

    roles are working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,10 @@ class PagesController < ApplicationController
     @zipcode = ""
     if current_user
       @problems = Problem.where(zip: current_user.zip)
+      @roles = current_user.roles
     else
       @problems = Problem.all
+      @roles = nil
     end
   end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -3,5 +3,9 @@ class Role < ApplicationRecord
   belongs_to :problem
   belongs_to :user
 
+  ## role level id 1 = ADMIN
+  ## role level id 2 = MODERATOR
+  ## role level id 3 = PARTICIPANT
+
   
 end
