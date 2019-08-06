05ea3ca8653611a8bc5206ad1588a13c304720c0
matthuhman <huhmanbeing@gmail.com>
2019-08-06 12:20:24 -0600

    push updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_144115) do
+ActiveRecord::Schema.define(version: 2019_08_03_165452) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -46,26 +46,31 @@ ActiveRecord::Schema.define(version: 2019_08_06_144115) do
   end
 
   create_table "problems", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "location"
-    t.datetime "target_completion_date"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "title", default: "Default Title. Change me!", null: false
+    t.text "description", default: "This is the default description. Please change me!", null: false
     t.string "city", default: "", null: false
     t.string "zip", default: "", null: false
     t.string "state", default: "", null: false
     t.string "country", default: "United States", null: false
+    t.decimal "latitude", precision: 18, scale: 15
+    t.decimal "longitude", precision: 18, scale: 15
+    t.datetime "target_completion_date"
+    t.integer "user_id"
+    t.integer "participants_required", default: 1
+    t.integer "current_participant_count", default: 1
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_problems_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
-    t.integer "role_level"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "level"
+    t.string "title"
     t.integer "user_id"
     t.integer "problem_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["problem_id"], name: "index_roles_on_problem_id"
     t.index ["user_id", "problem_id"], name: "index_roles_on_user_id_and_problem_id", unique: true
     t.index ["user_id"], name: "index_roles_on_user_id"
@@ -80,25 +85,37 @@ ActiveRecord::Schema.define(version: 2019_08_06_144115) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "first_name"
     t.string "last_name"
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "city", default: "", null: false
     t.string "zip", default: "", null: false
     t.string "state", default: "", null: false
     t.string "country", default: "United States", null: false
-    t.integer "followed_problems_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["followed_problems_id"], name: "index_users_on_followed_problems_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "zipcodes", force: :cascade do |t|
