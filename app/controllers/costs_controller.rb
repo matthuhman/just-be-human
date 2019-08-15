e53dde5551f09ee2a6a338fcbba6261260484f77
matthuhman <huhmanbeing@gmail.com>
2019-08-15 10:45:57 -0600

    starting to style up the costs page a bit, got charts working, also got the home page map to only show one infowindow at a time

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -1,7 +1,14 @@
 class CostsController < ApplicationController
 
   def display
-    @costs = Cost.get_monthly_costs
+    costs = Cost.get_monthly_costs
+
+    @cost_data = {}
+    @monthly_cost = costs.last.mtd_cost
+    costs.each do |c|
+      @cost_data[c.fetch_date.strftime('%Y-%m-%d')] = c.daily_cost
+    end
+
   end
 
 
