c651afa626db7c521a26a4a095b40492d18e10fb
matthuhman <huhmanbeing@gmail.com>
2019-10-05 11:36:27 -0600

    need to retest notifications for sure

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,4 +6,13 @@ class Notification < ApplicationRecord
   scope :unread, -> { where(read_at: nil) }
 
 
+  def stringify
+    if notifiable.class.name == "Post"
+      "#{actor.username} #{action} a post in #{notifiable.postable.title}"
+    elsif notifiable.class.name == "Requirement"
+      "#{actor.username} #{action} a requirement in #{notifiable.opportunity.title}"
+    elsif notifiable.class.name == "Opportunity"
+      "#{actor.username} #{action} for #{notifiable.title}"
+    end
+  end
 end
