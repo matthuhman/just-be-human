79aaf3b3a2966ddf3a3d9d28739d0925fe930cc6
matthuhman <huhmanbeing@gmail.com>
2020-02-12 20:12:29 -0700

    add leaderboard controller

---
+++ app/controllers/leaderboard_controller.rb
@@ -0,0 +1,7 @@
+class LeaderboardController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'leaderboard/show'
+  get 'leaderboard/index'
   root to: 'pages#home'
 
   get '/landing' => 'pages#landing'
